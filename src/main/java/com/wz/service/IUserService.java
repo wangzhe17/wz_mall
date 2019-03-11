3ed9cf0f3ae357b76c19873376a1235da94ef82c
wangzhe17 <1102472547@qq.com>
2019-03-11 11:12:19 +0800

    更新user

---
+++ src/main/java/com/wz/service/IUserService.java
@@ -0,0 +1,32 @@
+package com.wz.service;
+
+import com.wz.common.ServerResponse;
+import com.wz.pojo.User;
+import com.wz.common.ServerResponse;
+import com.wz.pojo.User;
+
+/**
+ * Created by geely
+ */
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer userId);
+
+    ServerResponse checkAdminRole(User user);
+}
